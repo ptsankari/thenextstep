84985cdb0fc2be10c16ff7dedc51baa52ec0adbd
sankari <ptsankari@gmail.com>
2021-08-24 19:00:35 +0530

    added new clubs

---
+++ samplephp.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>samplephp</title>
+</head>
+<body>
+<?php
+echo "Helloo World";
+?>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,4 @@
+var x="heyaaa what's uppppp";
+var a=20;
+var b=10;
+var c=a+b;
